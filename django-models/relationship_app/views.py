a9d743745413dd35d3c6a3c855de9217a040751c
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-08-17 19:39:48 +0300

    first task

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -39,3 +39,5 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
+    
+# UserCreationForm()
