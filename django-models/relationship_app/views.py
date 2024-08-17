8bfce946d8409012114ecf287374e08dcd3491ec
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-08-17 19:21:18 +0300

    first task

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
+
+def book_list_view(request):
+    books = Book.objects.all()
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(f"Books in the library:\n{book_list}")
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
