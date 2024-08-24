ae4a52d2fc2d4939659310f569219a240c31652d
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-08-25 02:16:39 +0300

    sec1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,9 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ['title', 'author', 'publication_year']
 #ExampleForm
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    published_date = forms.DateField(required=False)
