0c91d814494be0b0212a01039378e6f35a0e5e61
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-09-11 01:04:54 +0300

    0.1

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
+#api.urls
