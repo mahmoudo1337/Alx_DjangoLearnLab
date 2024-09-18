95d87d8c21415da945cf91162ff90db83b80ead1
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-09-18 23:17:54 +0300

    task1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+# blog/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import RegistrationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"Welcome, {user.username}! Your account has been created.")
+            return redirect('profile')
+    else:
+        form = RegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
